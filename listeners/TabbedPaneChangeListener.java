4ca0309a4f13bdc264c417afb2d8d791911e3322
Samuil Olegovich <47562089+SamuilOlegovich@users.noreply.github.com>
2020-10-06 13:12:41 +0300

    Update TabbedPaneChangeListener.java
    REF

--- listeners/TabbedPaneChangeListener.java
+++ listeners/TabbedPaneChangeListener.java
@@ -1,9 +1,7 @@
-package com.javarush.task.task32.task3209.listeners;
-
-import com.javarush.task.task32.task3209.View;
-
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+
+
 
 public class TabbedPaneChangeListener implements ChangeListener {
     private View view;
