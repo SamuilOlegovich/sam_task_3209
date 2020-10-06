0e559b936afe44ee419037b4affd2cde443e296a
Samuil Olegovich <47562089+SamuilOlegovich@users.noreply.github.com>
2020-10-06 13:00:53 +0300

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,13 +1,14 @@
-package com.javarush.task.task32.task3209;
-
 import javax.swing.text.html.HTMLDocument;
 import java.io.File;
+
+
 
 public class Controller {
     private View view;
     private HTMLDocument document;
     private File currentFile;
 
+    
     public static void main(String[] args) {
         View view = new View();
         Controller controller = new Controller(view);
